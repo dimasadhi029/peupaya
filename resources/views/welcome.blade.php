ab96a9363755eaf4dfd37afb54cbb9d5b2e0c203
Raflipratama9 <rafpratama9@gmail.com>
2019-07-31 12:54:49 +0700

    view1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Peupaya
                 </div>
 
                 <div class="links">
